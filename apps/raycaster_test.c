21f10a4ded0638ead4539a5d0f6955ff255c3e81
Vyacheslav Zhdanovskiy <vyacheslav.zhdanovskiy@nvi-research.com>
2023-04-12 17:35:22 +0300

    feat: hello_world

---
+++ apps/raycaster_test.c
@@ -0,0 +1,6 @@
+#include <raycaster_engine/raycaster_engine.h>
+
+int main(void) {
+    hello_world();
+    return 0;
+}
---
+++ src/raycaster_engine.c
@@ -0,0 +1,7 @@
+#include <raycaster_engine/raycaster_engine.h>
+
+#include <stdio.h>
+
+void hello_world() {
+    puts("Hello, world!");
+}
