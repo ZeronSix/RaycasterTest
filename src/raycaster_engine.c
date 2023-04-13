766d3a716064024d8f92dcfcb063977d589b2a1c
Vyacheslav Zhdanovskiy <vyacheslav.zhdanovskiy@nvi-research.com>
2023-04-13 11:10:25 +0300

    feat: add SDL2

--- src/raycaster_engine.c
+++ src/raycaster_engine.c
@@ -2,6 +2,28 @@
 
 #include <stdio.h>
 
-void hello_world() {
-    puts("Hello, world!");
+#include <SDL2/SDL.h>
+
+int raycaster_init() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
